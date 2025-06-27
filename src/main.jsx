dff02cfc13501a3d10894c049733facb8b40982a
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-06-27 14:45:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import './index.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+AOS.init({ duration: 800, once: true });
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);

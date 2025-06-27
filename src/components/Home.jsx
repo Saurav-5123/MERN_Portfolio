56836665c3571bff919d4ba65d95ac0d83f53800
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-06-27 11:10:54 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
+      <h1 className="text-5xl font-bold mb-4">Hi, I'm Saurav 👋</h1>
+      <p className="text-lg">I'm a Full Stack Web Developer</p>
+    </section>
+  );
+};
+
+export default Home;

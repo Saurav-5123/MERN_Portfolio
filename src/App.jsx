8fed7379e7b4d4934003d36c14f4969623dc0a4d
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-06-27 11:02:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;

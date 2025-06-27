f41067fb757cc07dd4031465e31fbc0fbaaaacce
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-06-27 13:57:15 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white shadow z-50 p-4 flex justify-center gap-6 font-semibold">
+    <nav className="fixed top-0 w-full bg-white shadow z-50 p-4 flex justify-center gap-6 font-semibold">
       <a href="#home" className="hover:text-blue-500">Home</a>
       <a href="#work" className="hover:text-blue-500">Work</a>
       <a href="#certifications" className="hover:text-blue-500">Certifications</a>

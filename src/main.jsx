b45b7ece1cc3cdc601c54a1c256d941ae0bcde0b
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-07-07 10:42:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import './index.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-AOS.init({ duration: 800, once: true });
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+AOS.init()
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)

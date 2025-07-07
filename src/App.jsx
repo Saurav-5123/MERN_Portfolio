632eca281a6a38b17d291801f09e787d78763453
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-07-07 10:43:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Work from "./components/Work";
-import Projects from "./components/Projects";
-import Certifications from "./components/Certifications";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Work from './components/Work'
+import Certifications from './components/Certifications'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
-const App = () => (
-  <div className="dark bg-gray-900 text-gray-100 font-sans">
-    <Navbar />
-    <main>
+const App = () => {
+  return (
+    <>
+      <Navbar />
       <Hero />
       <About />
       <Work />
-      <Projects />
       <Certifications />
       <Contact />
-    </main>
-    <Footer />
-  </div>
-);
+      <Footer />
+    </>
+  )
+}
 
-export default App;
+export default App

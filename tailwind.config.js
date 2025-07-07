4fbae9edecccc4d907b6a2bd165f036bdacd53d9
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-07-07 10:41:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
-module.exports = {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#111111",
+        secondary: "#1f1f1f",
+        accent: "#ff4d4d",
+      },
+    },
+  },
   plugins: [],
-};
+}
